d9d356e12eefbd6e08adfffd5ff7570e9afe045d
cv14 <spaudyal@gmail.com>
2018-03-11 17:09:19 -0400

    dd

---
+++ CMart/app/src/main/java/com/wordpress/cv14site/cmart/SecondAct.kt
@@ -0,0 +1,12 @@
+package com.wordpress.cv14site.cmart
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SecondAct : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
