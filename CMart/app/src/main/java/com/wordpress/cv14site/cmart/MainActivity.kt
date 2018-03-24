302faec093b66d060b421ec00f5416eed6000488
cv14 <spaudyal@gmail.com>
2018-03-24 18:36:05 -0400

    more prog

--- CMart/app/src/main/java/com/wordpress/cv14site/cmart/MainActivity.kt
+++ CMart/app/src/main/java/com/wordpress/cv14site/cmart/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mDrawerList = findViewById<ListView>(R.id.navList)
-
+        addDrawerItems()
     }
 
     private fun addDrawerItems() {
