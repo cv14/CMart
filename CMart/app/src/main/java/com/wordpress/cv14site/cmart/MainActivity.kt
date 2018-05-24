73f5073470921d53e5ac12cbf4ebb14336d29973
cv14 <spaudyal@gmail.com>
2018-05-24 18:04:44 -0400

    back at it

--- CMart/app/src/main/java/com/wordpress/cv14site/cmart/MainActivity.kt
+++ CMart/app/src/main/java/com/wordpress/cv14site/cmart/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mDrawerList = findViewById(R.id.navList)
+        mDrawerList = this.findViewById(R.id.navList)
         addDrawerItems()
     }
 
